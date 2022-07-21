3ac93113d2d92e8f11ef87999a0c0a7a0d63270a
Marhlon Korb <marhlonkorb@hotmail.com>
2022-07-21 20:25:37 -0300

    Início do projeto

---
+++ src/test/java/br/com/alura/apilinguagens/ApiLinguagensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.apilinguagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLinguagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
