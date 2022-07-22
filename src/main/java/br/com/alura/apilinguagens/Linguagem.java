08a90836d1d612ae2e093f9ee3e65ce36e38463a
Marhlon Korb <marhlonkorb@hotmail.com>
2022-07-21 23:14:57 -0300

    Criação do DataBase no MongoDB e conexão com api
    Nessa aula criamos uma API que utiliza a interface e dependências do MongoDB.
    Realizei o deploy do banco de dados no site do MongoDb e realizei a configuração da API com o banco para os dados inseridos no banco em formato JSON retornassem via requisição Http(GET ) no formato de lista

---
+++ src/main/java/br/com/alura/apilinguagens/Linguagem.java
@@ -0,0 +1,42 @@
+package br.com.alura.apilinguagens;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/*Annotation para identificar que no application.properties
+ há a string de conexão com a banco no MongoDb*/
+
+@Document(collection = "principaisLinguagens")
+public class Linguagem {
+    
+    @Id
+    private String id;
+    private String title;
+    private String image;
+    private int ranking;
+    
+    public Linguagem() {
+    
+    }
+    
+    public Linguagem(String title, String image, int ranking) {
+        this.title = title;
+        this.image = image;
+        this.ranking = ranking;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    public String getImage() {
+        return image;
+    }
+    public int getRanking() {
+        return ranking;
+    }
+
+}
